daa684e236d112cffdae77ef5a01d1437ff0f7d2
magumagu <powerofman26@gmail.com>
2024-09-03 15:36:55 +0900

    암호변경

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 
 const myLink = document.getElementById('myLink');
-const keyword = 'hisbeans'; // 패스워드
+const keyword = '참기름'; // 패스워드
 
 myLink.addEventListener('click', (event) => {
   event.preventDefault(); // 기본 링크 이동 방지
 
-  const inputValue = prompt('키워드를 입력하세요.');
+  const inputValue = prompt('세상에서 가장 긴 조미료 이름음?');
 
   if (inputValue === keyword) {
     window.location.href = myLink.href; // 링크로 이동
